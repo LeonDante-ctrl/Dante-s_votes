faadd2dcf83138b82835581ba3a2569e35319af0
Your Name <you@example.com>
2022-02-06 12:57:52 +0300

    added some validation code for the register section

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me ;)')
+class RegisterForm(FlaskForm):
+    email = StringField('email', validators=[InputRequired(), Email(message='Thats not right'), Length(max=50)])
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])    
 
 
 @app.route('/')
